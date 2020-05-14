117796c69dd7b99c1662e11436555f535a460119
Willian Oliveira <wijloc@gmail.com>
2020-05-14 18:32:39 -0300

    configure ssh keys

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,9 +1,12 @@
 #Git
 sudo apt-get install git-all
 
-git config --global user.email "wijloc@gmail.com"
+git config --global user.email "email@domain.com"
 
-git config --global user.name "Willian Oliveira"
+git config --global user.name "name name"
 
 #Vim
 sudo apt install vim
+
+#Insert new ssh key
+ssh-keygen -t rsa -b 4096 -C "email@domain.com"
